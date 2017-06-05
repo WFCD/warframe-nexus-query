ffc225e08c5b211f7255d730ec8074c5bfbc4dba
Matej Voboril <matej@voboril.org>
2017-06-04 19:14:40 -0500

    authentication and refactoring

--- index.js
+++ index.js
@@ -4,200 +4,13 @@ const JSONCache = require('json-fetch-cache');
 const Promise = require('bluebird');
 const md = require('node-md-config');
 const jsonQuery = require('json-query');
-const getColors = require('get-image-colors');
-const imageDownloader = require('image-downloader');
-
-const NexusItem = require('./lib/nexus/v1/item.js');
 const NexusFetcher = require('nexus-stats-api');
+
+const Settings = require('./lib/Settings.js');
+const NexusItem = require('./lib/nexus/v1/item.js');
 const MarketFetcher = require('./lib/market/v1/MarketFetcher.js');
+const AttachmentCreator = require('./lib/AttachmentCreator.js');
 
-const maxCacheLength = process.env.NEXUSSTATS_MAX_CACHED_TIME || 60000;
-const nexusKey = process.env.NEXUSSTATS_USER_KEY || undefined;
-const nexusSecret = process.env.NEXUSSTATS_USER_SECRET || undefined;
-
-const urls = {
-  nexus: process.env.NEXUSSTATS_URL_OVERRIDE || 'https://api.nexus-stats.com/warframe/v1/items',
-  market: process.env.MARKET_URL_OVERRIDE || 'https://api.warframe.market/v1/items',
-  marketAssets: process.env.MARKET_ASSETS_URL_OVERRIDE || 'https://warframe.market/static/assets/',
-};
-
-const defaultString = 'Operator, there is no such item pricecheck available.';
-
-const noResultAttachment = {
-  type: 'rich',
-  description: 'No result',
-  color: '0xff55ff',
-  url: 'https://warframe.market',
-  footer: {
-    text: 'Pricechecks from NexusStats and Warframe.Market',
-  },
-};
-
-/**
- * Calculate the safe value for a provided value
- * @param  {string} value provided value
- * @returns {string}       safe value
- */
-function safeValue(value) {
-  return value ? `${value}p` : 'No data';
-}
-
-/**
- * Calculate the safe value for a provided value
- * @param  {string} value1 provided minimum value
- * @param  {string} value2 provided maximum value
- * @returns {string}       safe value
- */
-function safeRange(value1, value2) {
-  return value1 ? `${value1}p - ${value2}p` : 'No data';
-}
-
-/**
- * Set colors for nexus components
- * @param  {NexusItem} components Array of nexus items
- * @returns {Array.<NexusItem>}    Array of nexus items with colors
- */
-function mapNexusColors(components) {
-  return new Promise((resolve) => {
-    const colored = [];
-    Promise.each(components, (component) => {
-      const coloredComponent = component;
-      const imgUrl = `https://nexus-stats.com/img/items/${encodeURIComponent(component.title)}-min.png`;
-      const options = {
-        url: imgUrl,
-        dest: `${__dirname}/tmp/${component.title}.png`,
-      };
-      imageDownloader.image(options)
-        // eslint-disable-next-line no-unused-vars
-        .then(({ fileName, image }) => getColors(image, 'image/png'))
-        .then((colors) => {
-          coloredComponent.color = typeof colors !== 'undefined' ? colors[0].hex().replace('#', '0x') : 0xff0000;
-          colored.push(coloredComponent);
-        });
-    })
-    .then(() => resolve(colored));
-  });
-}
-
-/**
- * Pad the left side of a string so that all componets
- * have the same string length before the pipe
- * @param {string} str the location string to pad
- * @param {number} length to make the string
- * @returns {string} the padded location string
- */
-function pad(str, length) {
-  let stringRet;
-  const len = length || 10;
-  if (str.length < len) {
-    stringRet = pad(`${str} `, len);
-  } else {
-    stringRet = str;
-  }
-  return stringRet;
-}
-
-function attachmentFromComponents(components, query) {
-  const attachment = {
-    type: 'rich',
-    title: query,
-    color: '0xff00ff',
-    url: '',
-    fields: [],
-    thumbnail: { url: '' },
-    footer: {
-      icon_url: '',
-      text: 'Price data provided by Nexus Stats & Warframe.Market',
-    },
-  };
-  const nexusComponents = components
-    .filter(component => component && component.components && component.components[0].type === 'nexus-v1');
-  const marketComponents = components
-    .filter(component => component && component.type === 'market-v1');
-
-  if (nexusComponents.length > 0) {
-    nexusComponents
-      .forEach((nexusComponent) => {
-        attachment.title = nexusComponent.title;
-        nexusComponent.components
-          .forEach((component) => {
-            let found = false;
-            attachment.thumbnail.url = `https://nexus-stats.com/img/items/${encodeURIComponent(nexusComponent.title)}-min.png`;
-            attachment.description = `Query results for: "${query}"`;
-            const nexusMedian = safeValue(component.prices.median);
-            const nexusRange = safeRange(component.prices.minimum, component.prices.maximum);
-            if (marketComponents.length > 0 && marketComponents[0].prices.soldPrice) {
-              marketComponents
-                .forEach((marketComponent) => {
-                  if (!found && marketComponent.name.indexOf(component.name) > -1) {
-                    found = true;
-                    attachment.color = marketComponent.color;
-                    attachment.url = marketComponent.url;
-                    const marketMedian = safeValue(marketComponent.prices.soldPrice);
-                    const marketRange = safeRange(marketComponent.prices.minimum,
-                      marketComponent.prices.maximum);
-
-                    attachment.fields.push({
-                      name: component.name,
-                      value: '```haskell\n' +
-                             `${pad('Value', 7)}|${pad(' Nexus', 13)}| Market\n` +
-                             `${pad('Median', 7)}|${pad(` ${nexusMedian}`, 13)}| ${marketMedian}\n` +
-                             `${pad('Range', 7)}|${pad(` ${nexusRange}`, 13)}| ${marketRange}\n\n` +
-                             `Trade Tax: ${marketComponent.tradingTax}cr\n` +
-                             '```\n',
-                      inline: true,
-                    });
-                  }
-                });
-            } else {
-              attachment.url = nexusComponent.url;
-              attachment.color = nexusComponent.color;
-              attachment.fields.push({
-                name: component.name,
-                value: '```haskell\n' +
-                       `${pad('Value', 7)}| ${pad('Nexus', 13)} | Market\n` +
-                       `${pad('Median', 7)}| ${pad(nexusMedian, 13)} | No data\n` +
-                       `${pad('Range', 7)}| ${pad(nexusRange, 13)} | No data\n\n` +
-                       '```\n',
-                inline: true,
-              });
-            }
-          });
-        attachment.fields.push({
-          name: '_ _',
-          value: `Supply: **${nexusComponent.supply.count}** units (${String((parseFloat(nexusComponent.supply.percentage) * 100).toFixed(2))}%) ` +
-            `- Demand: **${nexusComponent.demand.count}** units (${String((parseFloat(nexusComponent.demand.percentage) * 100).toFixed(2))}%)`,
-        });
-      });
-  } else if (marketComponents.length > 0) {
-    marketComponents
-      .forEach((marketComponent) => {
-        attachment.color = marketComponent.color;
-        attachment.title = marketComponent.name.replace(/\sset/i, '');
-        attachment.url = marketComponent.url;
-        attachment.thumbnail.url = marketComponent.thumbnail;
-        attachment.description = `Query results for: "${query}"`;
-
-        const marketMedian = safeValue(marketComponent.prices.soldPrice);
-        const marketRange = safeRange(marketComponent.prices.minimum,
-          marketComponent.prices.maximum);
-
-        attachment.fields.push({
-          name: marketComponent.name,
-          value: '```haskell\n' +
-                 `${pad('Value', 7)}|${pad(' Nexus', 13)}| Market\n` +
-                 `${pad('Median', 7)}|${pad(' No data', 13)}| ${marketMedian}\n` +
-                 `${pad('Range', 7)}|${pad(' No data', 13)}| ${marketRange}\n\n` +
-                 `Trade Tax: ${marketComponent.tradingTax}cr\n` +
-                 '```\n',
-          inline: true,
-        });
-      });
-  } else {
-    return noResultAttachment;
-  }
-  return attachment;
-}
 
 /**
  * Represents a queryable datastore of information derived from `https://nexus-stats.com/api`
@@ -208,25 +21,39 @@ class WarframeNexusStats {
    * @constructor
    */
   constructor() {
+    this.settings = new Settings();
+
     /**
      * The json cache storing data from nexus-stats.com
      * @type {JSONCache}
      */
-    this.nexusCache = new JSONCache(urls.nexus, maxCacheLength);
-
-    this.nexusFetcher = new NexusFetcher({
-      user_key: nexusKey,
-      user_secret: nexusSecret,
+    this.nexusCache = new JSONCache(this.settings.urls.nexus, this.settings.maxCacheLength);
+    const nexusOptions = {
+      user_key: this.settings.nexusKey,
+      user_secret: this.settings.nexusSecret,
       ignore_limiter: true,
-    });
+    };
+
+    this.nexusFetcher = new NexusFetcher(this.settings.nexusKey &&
+      this.settings.nexusSecret ? nexusOptions : {});
 
     /**
      * The json cache stpromg data from warframe.market
      * @type {JSONCache}
      */
-    this.marketCache = new JSONCache(urls.market, maxCacheLength);
+    this.marketCache = new JSONCache(this.settings.urls.market, this.settings.maxCacheLength);
 
+    /**
+     * Fetch market data
+     * @type {MarketFetcher}
+     */
     this.marketFetcher = new MarketFetcher();
+
+    /**
+     * Attachment creator for generating attachments
+     * @type {AttachmentCreator}
+     */
+    this.attachmentCreator = new AttachmentCreator();
   }
 
   /**
@@ -250,14 +77,14 @@ class WarframeNexusStats {
           }
 
           if (!results.value || JSON.stringify(results.value) === '[]') {
-            resolve([defaultString]);
+            resolve([this.settings.defaultString]);
           }
           this.nexusFetcher.getItemStats(results.value[0].name)
             .then((queryResults) => {
               if (Object.keys(queryResults).length > 0) {
                 componentsToReturn.push(new NexusItem(queryResults, `/${results.value[0].type}/${encodeURIComponent(results.value[0].name.replace(/\sPrime/ig, ''))}`));
               }
-              return mapNexusColors(componentsToReturn);
+              return this.attachmentCreator.mapNexusColors(componentsToReturn);
             })
             .then(components => resolve(components))
             // eslint-disable-next-line no-console
@@ -304,7 +131,7 @@ class WarframeNexusStats {
           });
           let componentsToReturnString = `${md.codeMulti}${tokens.join()}${md.blockEnd}`;
           componentsToReturnString = components.length > 0 ?
-            componentsToReturnString : defaultString;
+            componentsToReturnString : this.settings.defaultString;
           resolve(componentsToReturnString);
         });
     });
@@ -319,13 +146,7 @@ class WarframeNexusStats {
     return new Promise((resolve) => {
       this.priceCheckQuery(query)
         .then((components) => {
-          noResultAttachment.description = `No result for ${typeof query !== 'undefined' ? query : 'no search string'}`;
-          if ((components.length > 0 && components[0] === defaultString)
-              || components.length === 0) {
-            resolve([noResultAttachment]);
-            return;
-          }
-          resolve([attachmentFromComponents(components, query)]);
+          resolve([this.attachmentCreator.attachmentFromComponents(components, query)]);
         })
         // eslint-disable-next-line no-console
         .catch(console.error);
